c4f8d96281e191b493e06ae468efdf6394dc5ef9
Charlie Blackstock <charlieblackstock@gmail.com>
2025-05-17 14:58:34 -0600

    update fe

--- src/App.tsx
+++ src/App.tsx
@@ -23,13 +23,12 @@ export function App() {
 
       <Card className='bg-card/50 backdrop-blur-sm border-muted'>
         <CardContent className='pt-6'>
-          <h1 className='text-5xl font-bold my-4 leading-tight'>Bun + React</h1>
+          <h1 className='text-5xl font-bold my-4 leading-tight'> React</h1>
           <p>
             Edit{' '}
             <code className='relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm'>
               src/App.tsx
             </code>{' '}
-            and save to test HMR
           </p>
           <APITester />
         </CardContent>
